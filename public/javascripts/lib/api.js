d944101d95c5e45675bf05afe5cb7ba7cbd80e69
lidnnis <lidnnis@gmail.com>
2012-12-01 16:22:30 -0800

    Fixed callback/same origin policy for ajax

--- public/javascripts/lib/api.js
+++ public/javascripts/lib/api.js
@@ -3,7 +3,7 @@
 // tumblrAPI(process.argv[2])
 
 
-function flickrAPI(queryString, callback)
+function flickrAPI(queryString, callback, index)
 {
 arr = [];
  
@@ -12,7 +12,7 @@ $.getJSON(apiURL,function(data) {
 
    photo = data.photos.photo;
 
-   for (i=0; i < data.photos.photo.length; i++)
+   for (i=index; i < data.photos.photo.length; i++)
    {
     farm_id = photo[i].farm;
     server_id = photo[i].server;
@@ -26,6 +26,7 @@ $.getJSON(apiURL,function(data) {
      return replacements[all] || all;
    });
 
+    console.log(url);
     arr.push(url);
   }
   callback(arr);
@@ -37,7 +38,7 @@ function instagramAPI(queryString, callback)
 {
 arr = [];
 
-apiURL = "https://api.instagram.com/v1/tags/" + queryString + "/media/recent?access_token=35596053.f59def8.cc4fd7a9bb23496f91f072d02c76daea";
+apiURL = "https://api.instagram.com/v1/tags/" + queryString + "/media/recent?access_token=35596053.f59def8.cc4fd7a9bb23496f91f072d02c76daea&callback=?";
 $.getJSON(apiURL,function(data) {
 
   images = data.data;
@@ -50,7 +51,7 @@ $.getJSON(apiURL,function(data) {
         break;
 
     url = images[i].images.standard_resolution.url;
-
+    console.info(url);
     arr.push(url);
     }
     callback(arr);
@@ -61,7 +62,7 @@ function tumblrAPI(queryString, callback)
 {
 arr = [];
 
-apiURL = "https://api.tumblr.com/v2/tagged?tag=" + queryString + "&limit=10&api_key=bYPrsG99z2mJTSnp5HXiOD0N5zcCmG6wYgw8FBWC5MfSigw34M";
+apiURL = "https://api.tumblr.com/v2/tagged?tag=" + queryString + "&limit=10&api_key=bYPrsG99z2mJTSnp5HXiOD0N5zcCmG6wYgw8FBWC5MfSigw34M&callback=?";
 $.getJSON(apiURL,function(data) {
 
   images = data.response;
@@ -72,6 +73,7 @@ $.getJSON(apiURL,function(data) {
     if (images[i].photos != null)
     {
     url = images[i].photos[0].original_size.url;
+    console.info(url);
     arr.push(url);
     }
     }
